ef7d6190fc5946b2d35c7586c1c9f98503dd4d8c
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-02-24 22:07:55 +0330

    update conditions about capslk

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const passwordInput = $.querySelector("#password")
 const warningElem = $.querySelector("#warning")
 
 
+passwordInput.addEventListener('keyup', event => {
+
+    if (event.getModifierState("CapsLock") === true) {
+        warningElem.style.display = "block"
+    } else {
+        warningElem.style.display = "none"
+    }
+})
